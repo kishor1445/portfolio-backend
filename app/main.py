39760b50c3a4f745d43f0759c0d1ff192668c5d7
Kishor Ramanan <kishorramanan5@gmail.com>
2024-01-01 22:30:54 +0530

    v0.1

--- app/main.py
+++ app/main.py
@@ -1,7 +1,5 @@
-import os
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 from fastapi.staticfiles import StaticFiles
-import motor.motor_asyncio
 from dotenv import load_dotenv
 from .routers import data
 
@@ -11,5 +9,9 @@ app = FastAPI()
 
 for api in (data,):
     app.include_router(api.router)
+
+@app.get("/ping", status_code=status.HTTP_200_OK)
+def ping_test():
+    return {"message": "pong!"}
 
 app.mount(path="/static", app=StaticFiles(directory="static"), name="static")
