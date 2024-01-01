2f0b4eabfa5efa5cb542ef34915173f864b1f2e2
Kishor Ramanan <kishorramanan5@gmail.com>
2024-01-02 00:55:11 +0530

    v0.1

--- app/main.py
+++ app/main.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI, status
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 from .routers import data
 
 load_dotenv()
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 for api in (data,):
     app.include_router(api.router)
